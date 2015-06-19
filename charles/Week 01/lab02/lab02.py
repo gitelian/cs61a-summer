9d5fd6d2d440c90039307bd13f3238fe02964e39
Charles Frye <charlesfrye@berkeley.edu>
2015-06-19 12:59:38 -0700

    deleted extra files, moved my completed work to the right folders

---
+++ charles/Week 01/lab02/lab02.py
@@ -0,0 +1,88 @@
+"""Required questions for lab 2"""
+
+## Boolean Operators ##
+
+# Q6
+def both_positive(x, y):
+    """
+    Returns True if both x and y are positive.
+    >>> both_positive(-1, 1)
+    False
+    >>> both_positive(1, 1)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    return x > 0 and y > 0
+
+## while Loops ##
+
+# Q9
+def factors(n):
+    """Prints out all of the numbers that divide `n` evenly.
+
+    >>> factors(20)
+    20
+    10
+    5
+    4
+    2
+    1
+    """
+    div = n
+    while div > 0:
+        if n % div == 0:
+            print(div)
+        div -= 1
+
+
+# Q10
+def fib(n):
+    """Returns the nth Fibonacci number.
+
+    >>> fib(0)
+    0
+    >>> fib(1)
+    1
+    >>> fib(2)
+    1
+    >>> fib(3)
+    2
+    >>> fib(4)
+    3
+    >>> fib(5)
+    5
+    >>> fib(6)
+    8
+    """
+    cur, prev = 1,0
+    ct = 0
+    while ct < n:
+        cur,prev = cur+prev,cur
+        ct+=1
+    print(prev)
+
+def gets_discount(x, y):
+    """ Returns True if this is a combination of a senior citizen
+    and a child, False otherwise.
+
+    >>> gets_discount(65, 12)
+    True
+    >>> gets_discount(9, 70)
+    True
+    >>> gets_discount(40, 45)
+    False
+    >>> gets_discount(40, 75)
+    False
+    >>> gets_discount(65, 13)
+    False
+    >>> gets_discount(7, 9)
+    False
+    >>> gets_discount(73, 77)
+    False
+    >>> gets_discount(70, 31)
+    False
+    >>> gets_discount(10, 25)
+    False
+    """
+    return (x >= 65 and y <= 12) or (x <= 12 and y >= 65)
+

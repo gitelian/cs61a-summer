7af9c51e452f2969bbbeb6d9c57eb70e9e6ca428
Charles Frye <charlesfrye@berkeley.edu>
2015-06-30 13:30:20 -0700

    Completed Lab 09

--- charles/Week 08/lab09/lab09.scm
+++ charles/Week 08/lab09/lab09.scm
@@ -1,20 +1,29 @@
 (define (cube x)
-  'YOUR-CODE-HERE
+  (* x x x)
 )
 
 (define (over-or-under x y)
-  'YOUR-CODE-HERE
+  (if (< x y)
+    -1
+  	(if (> x y)
+  	 1
+  	 0))
 )
 
 (define (make-adder num)
-  'YOUR-CODE-HERE
+  (lambda (x) (+x num))
 )
 
 (define structure
-  'YOUR-CODE-HERE
+  (cons (cons 1 '())
+  	(cons 2
+  		(cons (cons 2 4)
+  			(cons 5 '()))))
 )
 
 (define (remove item lst)
-  'YOUR-CODE-HERE
+  (cond ((null? lst) '())
+  	((equal? item (car lst)) (remove item (cdr lst)))
+  	(else (cons (car lst) (remove item (cdr lst)))))
 )
 
